029c51d71f4a2fa97675b3b460682cd527707218
AleixMJ <aleixtechIT@gmail.com>
2022-09-12 17:32:45 +0100

    linked correctly Flask, with Database and html

--- app.py
+++ app.py
@@ -5,25 +5,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data = get_db()
-    return str(data)
 
+    user = query_db('select * from users where id = ?',[1], one=True)
+
+    if user is None:
+        print('No such user')
+    else:
+        username = user[1]
+
+    return render_template("index.html", username=username)
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect("cryptotax.db")
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM users")
-    return cursor.fetchall()
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
-
-
-if __name__ == "_main__":
-    app.run
-    
